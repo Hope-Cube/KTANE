303ee242cb65ba316d7c68b81a88fd50723f00a4
Hope-Cube <horvathhunor1@gmail.com>
2023-12-13 08:34:05 +0100

    Created Needy_module class.

---
+++ KTANE/Needy_module.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTANE
+{
+    /// <summary>
+    /// Timed modules that must do in time.
+    /// </summary>
+    internal class Needy_module
+    {
+        public class Venting_gas
+        {
+
+        }
+        public class Capacitor_discharge
+        {
+
+        }
+        public class Knobs
+        {
+
+        }
+    }
+}
