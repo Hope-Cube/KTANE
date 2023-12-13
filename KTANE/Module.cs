7ec2578b006a007a743687e578e05a8a900ec248
Hope-Cube <horvathhunor1@gmail.com>
2023-12-13 08:28:37 +0100

    Created Module class.

---
+++ KTANE/Module.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTANE
+{
+    internal class Module
+    {
+        public class Wires
+        {
+
+        }
+        public class Button
+        {
+
+        }
+        public class Keypads
+        {
+
+        }
+        public class Simon_says
+        {
+
+        }
+        public class Whos_on_first
+        {
+
+        }
+        public class Memory
+        {
+
+        }
+        public class Morse_code
+        {
+
+        }
+        public class Comp_wires
+        {
+
+        }
+        public class Wire_seqs
+        {
+
+        }
+        public class Maze
+        {
+
+        }
+        public class Passwords
+        {
+
+        }
+    }
+}
