d653d096398a3936f7f76e3f0621a12e7158e498
Hope-Cube <hopecube@gmail.com>
2023-12-16 00:44:53 +0100

    Maze progress.

--- KTANE/Module.cs
+++ KTANE/Module.cs
@@ -157,8 +157,8 @@ namespace KTANE
                 // Print the path
                 if (path != null)
                 {
-                    WriteLine("Path found:");
-                    Bitmap pathBitmap = new Bitmap(11, 11);
+                    //WriteLine("Path found:");
+                    //Bitmap pathBitmap = new Bitmap(11, 11);
                     for (int i = 0; i < path.Count - 1; i += 2)
                     {
                         AStarNode currentNode = path[i];
@@ -166,21 +166,21 @@ namespace KTANE
                         int dx = (nextNode != null) ? nextNode.X - currentNode.X : 0;
                         int dy = (nextNode != null) ? nextNode.Y - currentNode.Y : 0;
                         string direction = GetDirection(dx, dy);
-                        WriteLine($"{direction}");
+                        //WriteLine($"{direction}");
                         s += direction;
-                        pathBitmap.SetPixel(currentNode.X, currentNode.Y, Color.FromArgb(0, 255, 255));
-                        if (nextNode != null)
-                        {
-                            pathBitmap.SetPixel(nextNode.X, nextNode.Y, Color.FromArgb(0, 255, 255));
-                        }
+                        //pathBitmap.SetPixel(currentNode.X, currentNode.Y, Color.FromArgb(0, 255, 255));
+                        //if (nextNode != null)
+                        //{
+                        //    pathBitmap.SetPixel(nextNode.X, nextNode.Y, Color.FromArgb(0, 255, 255));
+                        //}
                     }
                     // Add the last node to the bitmap
-                    pathBitmap.SetPixel(path.Last().X, path.Last().Y, Color.FromArgb(0, 255, 255));
-                    pathBitmap.Save("path.png");
+                    //pathBitmap.SetPixel(path.Last().X, path.Last().Y, Color.FromArgb(0, 255, 255));
+                    //pathBitmap.Save("path.png");
                 }
                 else
                 {
-                    WriteLine("No path found.");
+                    System.Windows.MessageBox.Show("No path found.", "Warning!");
                 }
                 return s;
             }
